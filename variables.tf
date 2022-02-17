1ca2022e50577ab46cf72cbf4104362864eb972c
hyeinee3 <86913926+hyeinee3@users.noreply.github.com>
2022-02-17 11:05:37 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
  variable "ec2_name" {
    description = "ec2 instance tag Name"
-   default = "DevOS_terraform_by hyein (Core) - 생성확인"
+   default = "DevOS_terraform_by hyein (Core)"
  }
 
  variable "subnet_id" {
